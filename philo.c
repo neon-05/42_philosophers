3b536d6fe29f8d90d500e7576dbd6f5d99d68a7b
Yohann Labussiere <ylabussi@c1r2s4.42lausanne.ch>
2025-02-18 13:02:31 +0100

    added automated tests

--- philo.c
+++ philo.c
@@ -6,13 +6,13 @@
 /*   By: ylabussi <ylabussi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 18:21:57 by ylabussi          #+#    #+#             */
-/*   Updated: 2025/02/13 16:47:23 by ylabussi         ###   ########.fr       */
+/*   Updated: 2025/02/17 16:10:36 by ylabussi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	philo_start(t_philo *philos, t_table *table)
+int	philo_start(t_philo *philos, t_table *table)
 {
 	size_t	i;
 
@@ -24,19 +24,20 @@ void	philo_start(t_philo *philos, t_table *table)
 		philos[i].table = table;
 		pthread_create(&(philos[i].tid), NULL, &lifeofaphilo, &(philos[i]));
 		i++;
-		i++;
 	}
 	while (i > 0)
 		pthread_join(philos[--i].tid, NULL);
 	if (table->deadphilo)
 		printf("%6li %3li died \n",
-			usecsince(table->start) / 1000, table->deadphilo - 1);
+			usecsince(table->start) / 1000, table->deadphilo);
+	return (table->deadphilo != 0);
 }
 
 int	main(int argc, const char *argv[])
 {
 	size_t	i;
 	int		e;
+	int		status;
 	t_philo	*philos;
 	t_table	*table;
 
@@ -44,14 +45,15 @@ int	main(int argc, const char *argv[])
 		return (1);
 	e = 0;
 	i = 0;
+	status = 1;
 	while (i < table->nphilos)
 		e |= pthread_mutex_init(&(table->forks[i++]), NULL);
 	if (!e)
-		philo_start(philos, table);
+		status = philo_start(philos, table);
 	while (i > 0)
 		pthread_mutex_destroy(&(table->forks[--i]));
 	free(philos);
 	free(table->forks);
 	free(table);
-	return (0);
+	return (status);
 }
--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-( ./philo $2 $3 $4 $5 $6 | grep died || echo $1 good) & if [ $1 -gt 0 ]; then $0 $(($1-1)) $2 $3 $4 $5 $6; fi
+LOG="${7:-/dev/null}"
+( if ./philo $2 $3 $4 $5 $6 >> $LOG; then echo $1 "\033[32;1mOK\033[0m"; else echo $1 "\033[31;1mKO\033[0m"; fi ) & if [ $1 -gt 1 ]; then $0 $(($1-1)) $2 $3 $4 $5 $6 $7; fi
